355c9f9e3d017dd172a308d88a21248bfcadb156
Milin28 <milin.buas@bumail.net>
2026-02-13 17:02:58 +0700

    v1

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,8 +4,14 @@ public class Food : MonoBehaviour
 {
     public int attackPoint = 5;
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-
+        Debug.Log("hit" + other.gameObject.name);
+        
+        if (other.gameObject.TryGetComponent<HealthV1>(out HealthV1 health))
+        {
+            health.TakeDamage(attackPoint);
+        }
+        Destroy(gameObject);
     }
 }
